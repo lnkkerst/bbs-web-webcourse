7d8229b529e219a58e90479b84433346615b2b36
lnkkerst <lnkkerst04@gmail.com>
2024-01-21 17:48:38 +0800

    feat: first page loading

--- app.vue
+++ app.vue
@@ -12,9 +12,20 @@ useHead({
 
 <template>
   <div>
-    <NuxtLoadingIndicator color="#ffffffaa"></NuxtLoadingIndicator>
-    <NuxtLayout>
-      <NuxtPage></NuxtPage>
-    </NuxtLayout>
+    <Suspense>
+      <template #default>
+        <div>
+          <NuxtLoadingIndicator color="#ffffffaa"></NuxtLoadingIndicator>
+          <NuxtLayout>
+            <NuxtPage></NuxtPage>
+          </NuxtLayout>
+        </div>
+      </template>
+      <template #fallback>
+        <div class="absolue z-114514 top-0 left-0 right-0 bottom-0">
+          <q-inner-loading showing color="primary" dark></q-inner-loading>
+        </div>
+      </template>
+    </Suspense>
   </div>
 </template>
